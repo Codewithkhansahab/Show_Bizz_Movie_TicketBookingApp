6afc1fcfc42e3b30bda878b7cfc9674c177ced85
Tousif Khan <154345094+Codewithkhansahab@users.noreply.github.com>
2025-08-31 14:34:43 +0530

    Update HeaderSection.jsx

--- client/src/components/HeaderSection.jsx
+++ client/src/components/HeaderSection.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 const HeaderSection = () => {
     const navigate = useNavigate()
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/saiyaara2.jpeg")] bg-cover bg-center h-screen '>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/122316334.png")] bg-cover bg-center h-screen '>
         {/* <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20'/> */}
         <h1 className='text-5xl md"text-[70px] md:leading-18 font-semibold max-w-110 text-black'>Saiyara <br /> </h1>
         <div className='flex items-center gap-4 text-black'>
